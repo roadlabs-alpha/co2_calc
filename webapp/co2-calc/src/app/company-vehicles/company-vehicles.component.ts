a38c3c296bf0a877f9f99206009f6a7f4d32f1aa
Lukas Merkle <lukas.merkle@live.de>
2021-04-01 19:29:27 +0200

    added group numbering and deleting from list

--- webapp/co2-calc/src/app/company-vehicles/company-vehicles.component.ts
+++ webapp/co2-calc/src/app/company-vehicles/company-vehicles.component.ts
@@ -19,19 +19,10 @@ export class CompanyVehiclesComponent implements OnInit{
 	is_private_use = false;
 	is_pool_car = false;
 	n_total_vehicles=0;
-	vgn=0;
-
-	vehicle_groupsaaa=[{
-		"vehicleclass": "none",
-		"vehicleprop": "none",
-		"mileage": "none",
-		"count": "none",
-		"is_poolcar": "none",
-		"is_private_use": "none",
-	}]
+	
 
 	vehicle_groups: Array<VehicleGroup>=[];
-
+	vgn=this.generate_vehicle_group_name(0);
 
 
 	fg_vehicleclass = new FormGroup({
@@ -49,6 +40,24 @@ export class CompanyVehiclesComponent implements OnInit{
 	ngOnInit(): void {
 	}
 
+	generate_vehicle_group_name(idx_add: number): string{
+
+		var fist_guess = "Vehicle group "+String(this.vehicle_groups.length+1)
+
+		var vgns=[];
+		for (var i=0; i<this.vehicle_groups.length; i++){
+			vgns.push(this.vehicle_groups[i].vgn)
+		}
+
+		var idx=0;
+		while (vgns.includes(fist_guess)){
+			fist_guess = "Vehicle group "+String(this.vehicle_groups.length+1+idx+idx_add)
+			idx+=1;
+		}
+
+		return fist_guess
+	}
+
 
 	add_vehicle_group(): void{
 
@@ -60,7 +69,9 @@ export class CompanyVehiclesComponent implements OnInit{
 		// console.log(this.fg_vehicleclass.value.fc_poolcar);
 		// console.log(this.fg_vehicleclass.value.fc_privateuse);
 
-		var vg = new VehicleGroup();
+		var vgn = this.generate_vehicle_group_name(0);
+		var vg = new VehicleGroup(vgn);
+		
 		vg.vehicleclass = this.fg_vehicleclass.value.fc_vehicleclass;
 		vg.vehicleprop = this.fg_vehicleclass.value.fc_vehicleprop;
 		vg.mileage = this.fg_vehicleclass.value.fc_mileage;
@@ -71,28 +82,28 @@ export class CompanyVehiclesComponent implements OnInit{
 		this.vehicle_groups.push(vg);
 
 		this.calc_total_vehicle_count()
+		this.vgn=this.generate_vehicle_group_name(1);
 
+	}
+
+	delete_vehicle_group(vgi: number): void{
+
+		console.log("del element", vgi)
+		this.vehicle_groups.splice(vgi,1);
+		this.vgn=this.generate_vehicle_group_name(0);
 	}
 
 	calc_total_vehicle_count(): void{
 
 		this.n_total_vehicles=0;
 		for (var i=0; i<this.vehicle_groups.length; i++){
-
-			console.log(parseInt("3"));
-			console.log(typeof(this.vehicle_groups[i].count));
-			console.log(Number(this.vehicle_groups[i].count));
-			
 			this.n_total_vehicles += Number(this.vehicle_groups[i].count);
-		
 		}
-		console.log(this.vehicle_groups);
-
 	}
-
 }
 
 class VehicleGroup{
+	vgn="";
 	is_private_use=0;
 	is_poolcar=0;
 	vehicleclass=0;
@@ -100,5 +111,7 @@ class VehicleGroup{
 	mileage=0;
 	count=0;
 
-	constructor() { }
+	constructor(vgn: string) { 
+		this.vgn=vgn
+	}
 }
