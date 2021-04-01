4d29836bbd0dab79193959b1bfc391a3628e7f04
Lukas Merkle <lukas.merkle@live.de>
2021-04-01 17:29:12 +0200

    added vehicle groups

--- webapp/co2-calc/src/app/company-vehicles/company-vehicles.component.ts
+++ webapp/co2-calc/src/app/company-vehicles/company-vehicles.component.ts
@@ -11,6 +11,7 @@ import { FormControl, FormGroup } from '@angular/forms'
 })
 
 
+
 export class CompanyVehiclesComponent implements OnInit{
 
 
@@ -20,20 +21,26 @@ export class CompanyVehiclesComponent implements OnInit{
 	n_vehicles=0;
 	vgn=0;
 
+	vehicle_groupsaaa=[{
+		"vehicleclass": "none",
+		"vehicleprop": "none",
+		"mileage": "none",
+		"count": "none",
+		"is_poolcar": "none",
+		"is_private_use": "none",
+	}]
+
+	vehicle_groups: Array<VehicleGroup>=[];
+
 
 
 	fg_vehicleclass = new FormGroup({
 		fc_vehicleclass: new FormControl(''),
 		fc_vehicleprop: new FormControl(''),
-		fc_mileage: new FormControl("")
-	});
-
-	fg_checks = new FormGroup({
+		fc_mileage: new FormControl(""),
 		fc_poolcar: new FormControl(false),
 		fc_privateuse: new FormControl(false),
-	});
-
-	fg_add = new FormGroup({
+		fc_count: new FormControl(0),
 		fc_add: new FormControl(false),
 	});
 
@@ -42,4 +49,43 @@ export class CompanyVehiclesComponent implements OnInit{
 	ngOnInit(): void {
 	}
 
+
+	add_vehicle_group(): void{
+
+		console.log("add vehicle group")
+		console.log(this.fg_vehicleclass.value.fc_vehicleclass);
+		console.log(this.fg_vehicleclass.value.fc_vehicleprop);
+		console.log(this.fg_vehicleclass.value.fc_mileage);
+		console.log(this.fg_vehicleclass.value.fc_count);
+		console.log(this.fg_vehicleclass.value.fc_poolcar);
+		console.log(this.fg_vehicleclass.value.fc_privateuse);
+
+		var vg = new VehicleGroup();
+		vg.is_private_use = this.fg_vehicleclass.value.fc_vehicleclass;
+		vg.is_poolcar = this.fg_vehicleclass.value.fc_vehicleprop;
+		vg.vehicleclass = this.fg_vehicleclass.value.fc_mileage;
+		vg.vehicleprop = this.fg_vehicleclass.value.fc_count;
+		vg.mileage = this.fg_vehicleclass.value.fc_poolcar;
+		vg.count = this.fg_vehicleclass.value.fc_privateuse;
+		//var vg = "as";
+		this.vehicle_groups.push(vg);
+
+		
+
+
+		console.log(this.vehicle_groups);
+
+	}
+
+}
+
+class VehicleGroup{
+	is_private_use=0;
+	is_poolcar=0;
+	vehicleclass=0;
+	vehicleprop=0;
+	mileage=0;
+	count=0;
+	
+	constructor() { }
 }
