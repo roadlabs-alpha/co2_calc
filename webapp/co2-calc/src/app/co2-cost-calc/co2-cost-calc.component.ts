4b02fbd0e615fcfbf5a3c9572b8502a8b9b79364
Lukas Merkle <lukas.merkle@live.de>
2021-04-04 10:32:23 +0200

    tidy

--- webapp/co2-calc/src/app/co2-cost-calc/co2-cost-calc.component.ts
+++ webapp/co2-calc/src/app/co2-cost-calc/co2-cost-calc.component.ts
@@ -82,20 +82,20 @@ export class Co2CostCalcComponent implements OnInit {
 		for(var i=0; i < this.state.vehicle_groups_user.length; i++){
 			console.log("Doing vehicle group", i)
 
-			//var vehicle = new Vehicle(String(this.state.vehicle_groups_user[i].vehicleprop), String(this.state.vehicle_groups_user[i].vehicleclass))
-			
-			//var mean_mileage = (this.state.vehicle_groups_user[i].mileage[0] + this.state.vehicle_groups_user[i].mileage[1])/2
-
-			//vehicle.do_tco(mean_mileage)
-			//console.log(vehicle.total_yearly_co2)
-			//console.log("Co2 per km: ", vehicle.total_co2_per_km, " kg/km.")
-			//console.log("Cost per km: ", vehicle.total_cost_per_km, " €/km.")
-
 			console.log("Co2/year of group in kg ", i, ": ",this.state.vehicle_groups_user[i].calculate_vg_co2())
 			console.log("Cost/year of group in € ", i, ": ",this.state.vehicle_groups_user[i].calculate_vg_cost())
 
 		}
 	}
+
+
+
+
+
+
+
+	// - Business trips --------------------------------------------------
+	//this.state.bt_value_set_user
 
 
 	calc_all(): void{
@@ -114,105 +114,3 @@ export class Co2CostCalcComponent implements OnInit {
 	}
 
 }
-
-
-
-
-// export class Vehicle{
-
-// 	writeoff_period=0;
-// 	workshop_cost=0;
-// 	fixcost=0;
-// 	consumption=0;
-// 	tech: string;
-// 	emissions_per_km
-// 	new_price: number=0;
-// 	residual_value: number=0;
-// 	yearly_mileage: number=0;
-// 	total_yearly_cost
-// 	total_cost_per_km
-// 	total_yearly_co2
-// 	total_co2_per_km
-
-// 	data = new Data();
-// 	dataService
-
-
-// 	constructor(tech: string, vehicle_class: string){
-
-// 		this.dataService = new DataService()
-
-// 		this.getData()
-
-// 		this.writeoff_period = 3 // years
-// 		this.workshop_cost = 720 //€/year
-// 		this.fixcost = 1200 //€/year
-
-// 		if (tech=="electric"){
-// 			this.tech="bev";
-// 		}else{
-// 			this.tech = tech;
-// 		}
-
-		
-
-
-
-// 		this.emissions_per_km = this.data.emissions_per_km.get(this.tech)
-
-
-
-// 		var vehicle_class_detail = this.data.vehicle_class.get(vehicle_class)
-// 		if (vehicle_class_detail != undefined){
-// 			this.new_price = vehicle_class_detail.price_new
-// 			this.residual_value  = vehicle_class_detail.residual_value3y
-
-// 			if (this.tech == "bev"){
-// 				this.consumption = vehicle_class_detail.e_consumption;
-// 			}
-// 			else{
-// 				this.consumption = vehicle_class_detail.consumption;
-// 			}
-// 		}
-
-// 		this.total_yearly_cost = -1
-// 		this.total_cost_per_km = -1
-// 		this.total_yearly_co2 = -1
-// 		this.total_co2_per_km = -1
-
-
-// 	}
-
-// 	do_tco(yearly_mileage: number){
-
-// 		var value_loss = 0
-// 		var yearly_loss=0
-// 		var energy_cost_yearly=0
-
-// 		this.yearly_mileage = yearly_mileage
-
-// 		value_loss = this.new_price - this.residual_value
-// 		yearly_loss = value_loss / this.writeoff_period
-
-		
-// 		var ep = this.data.energy_price.get(this.tech)
-// 		if (ep != undefined){
-// 			energy_cost_yearly = yearly_mileage * this.consumption / 100 * ep
-// 		}
-
-// 		this.total_yearly_cost = this.workshop_cost + this.fixcost + yearly_loss + energy_cost_yearly
-// 		this.total_cost_per_km = this.total_yearly_cost / yearly_mileage
-		
-// 		var epk = this.data.emissions_per_energy.get(this.tech)
-// 		if (epk != undefined){
-// 			this.total_yearly_co2 = yearly_mileage * this.consumption / 100 * epk
-// 		}
-// 		this.total_co2_per_km = this.total_yearly_co2 / yearly_mileage
-
-// 	}
-
-// 	getData(): void {
-// 		this.dataService.getData().subscribe(dat => this.data = dat);
-// 	}
-
-// }
