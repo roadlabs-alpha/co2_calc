1e32755bb86990de4266110fa46b7779e8260fcc
Lukas Merkle <lukas.merkle@live.de>
2021-04-05 20:14:26 +0200

    addedclass Commute_Group holding the commuting groups and their shares.

--- webapp/co2-calc/src/app/company-commuting/company-commuting.component.ts
+++ webapp/co2-calc/src/app/company-commuting/company-commuting.component.ts
@@ -61,3 +61,65 @@ export class CompanyCommutingComponent implements OnInit {
 		this.dataService.getData().subscribe(dat => this.data = dat);
 	}
 }
+
+
+
+export class Commute_Group{
+
+	name="";
+	mode="";
+	mode_name="";
+	trip_count=0;
+	share=0;
+	avg_distance: Array<number>=[]
+	avg_distance_name=""
+
+	co2=0;
+	cost=0;
+
+	data = new Data();
+	dataService
+
+	constructor(name: string, mode: string, mode_name: string, count: number, dist: Array<number>, dist_name: string, share: number){
+
+		this.name = name; // groupname
+		this.mode = mode; // mode (pt, train, car ...)
+		this.mode_name = mode_name; // (well written name Public Transport, etc)
+		this.trip_count = count;
+		this.avg_distance = dist;
+		this.avg_distance_name = dist_name;
+		this.share = share;
+
+		this.dataService = new DataService()
+		this.getData()
+
+	}
+
+
+	calculate_btg_co2(): number{
+
+		var mean_dist = (this.avg_distance[0] + this.avg_distance[1])/2
+		var epk = this.data.emissions_per_km.get(this.mode)
+		console.log("epk: ", epk, "mode: ", this.mode)
+		if (epk != undefined){
+			this.co2 = this.trip_count * mean_dist * epk
+		}
+
+		return this.co2
+	}
+
+	calculate_btg_cost(): number{
+		var mean_dist = (this.avg_distance[0] + this.avg_distance[1])/2
+		var tpk = this.data.transport_price_per_km.get(this.mode)
+
+		if (tpk != undefined){
+			this.cost= this.trip_count * mean_dist * tpk
+		}
+		return this.cost
+	}
+
+	getData(): void {
+		this.dataService.getData().subscribe(dat => this.data = dat);
+	}
+
+}
